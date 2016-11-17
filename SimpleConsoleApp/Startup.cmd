7a907d03f4ec3be2d66609fdbe35a1f2eeb1896f
Sergey Kanzhelev <SergKanz@microsoft.com>
2016-11-17 15:12:27 -0800

    first version

---
+++ SimpleConsoleApp/Startup.cmd
@@ -0,0 +1,9 @@
+SET COR_ENABLE_PROFILING=1
+SET COR_PROFILER={324F817A-7420-4E6D-B3C1-143FBED6D855}
+SET COR_PROFILER_PATH=RTIA\x64\MicrosoftInstrumentationEngine_x64.dll
+SET MicrosoftInstrumentationEngine_HostPath=%cd%\RTIA\x64\Microsoft.ApplicationInsights.ExtensionsHost_x64.dll
+SET MicrosoftInstrumentationEngine_Host={CA487940-57D2-10BF-11B2-A3AD5A13CBC0}
+SET MicrosoftInstrumentationEngine_MessageboxAtAttach=1
+SET MicrosoftInstrumentationEngine_FileLog="Errors|Dumps"
+
+bin\x64\Debug\SimpleConsoleApp.exe
