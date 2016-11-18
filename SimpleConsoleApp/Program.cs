b46722c406808f3bb0576ab46f2b5f6c97bb6a4a
Sergey Kanzhelev <SergKanz@microsoft.com>
2016-11-17 16:32:37 -0800

    all comments are there

--- SimpleConsoleApp/Program.cs
+++ SimpleConsoleApp/Program.cs
@@ -1,11 +1,16 @@
-﻿using Microsoft.Diagnostics.Instrumentation.Extensions.Intercept;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
+using System.Net.Mail;
 using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
+
+using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.Channel;
+using Microsoft.ApplicationInsights.DataContracts;
+using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.ApplicationInsights.Extensibility.Implementation;
+using Microsoft.Diagnostics.Instrumentation.Extensions.Intercept;
 
 namespace SimpleConsoleApp
 {
@@ -18,6 +23,8 @@ namespace SimpleConsoleApp
                 Console.WriteLine("Please run this application using Startup.cmd script.");
                 return;
             }
+
+            TelemetryConfiguration.Active.InstrumentationKey = "test";
 
             Console.WriteLine("Agent version: " + Decorator.GetAgentVersion());
 
@@ -36,8 +43,12 @@ namespace SimpleConsoleApp
         {
             try
             {
-                System.Net.Mail.SmtpClient client = new System.Net.Mail.SmtpClient();
-                client.Send(null);
+                System.Net.Mail.SmtpClient client = new System.Net.Mail.SmtpClient("host.myserver.abc", 443);
+                client.Send(new MailMessage(
+                    from: "sergkanz@microsoft.com", 
+                    to: "chucknorris@microsoft.com", 
+                    subject: "This is RTIA example", 
+                    body: "Check out this example on github"));
             }
             catch (Exception)
             {
@@ -47,24 +58,51 @@ namespace SimpleConsoleApp
 
         public static object OnBegin(object thisObj, object arg1)
         {
+            // diagnostics output
             Console.WriteLine("Begin callback");
             Console.WriteLine("Callstack: " + new StackTrace().ToString());
 
-            return null;
+            // start the operation
+            var operation = new TelemetryClient().StartOperation<DependencyTelemetry>("Send");
+            operation.Telemetry.Target = ((SmtpClient)thisObj).Host;
+            if (arg1 != null)
+            {
+                operation.Telemetry.Data = ((MailMessage)arg1).Subject;
+            }
+
+            // save the operation in the local context
+            return operation;
         }
 
         public static object OnEnd(object context, object returnValue, object thisObj, object arg1)
         {
+            // diagnostics output
             Console.WriteLine("End callback");
             Console.WriteLine("Callstack: " + new StackTrace().ToString());
 
+            // stop the operation. Getting the operation from the context
+            var operation = (IOperationHolder<DependencyTelemetry>)context;
+            new TelemetryClient().StopOperation(operation);
+
+            // you must return original return value unless you want to change it
             return returnValue;
         }
 
         public static void OnException(object context, object exception, object thisObj, object arg1)
         {
+            // diagnostics output
             Console.WriteLine("Exception callback");
             Console.WriteLine("Callstack: " + new StackTrace().ToString());
+
+            // mark operation as failed and stop it. Getting the operation from the context
+            var operation = (IOperationHolder<DependencyTelemetry>)context;
+            operation.Telemetry.Success = false;
+            operation.Telemetry.ResultCode = exception.GetType().Name;
+            new TelemetryClient().StopOperation(operation);
+
+            // this is just to trace the result:
+            Console.WriteLine("Telemetry item: " + Encoding.Default.GetString(
+                JsonSerializer.Serialize(new List<ITelemetry>() { operation.Telemetry }, false)));
         }
     }
 }
